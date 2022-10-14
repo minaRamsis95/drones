4e623b513de5440cc4d51b449afe7a313a5b833f
Mina <mina.ramsis95@yahoo.com>
2022-10-14 11:59:34 +0200

    Basic project setup with connection to mongodb

--- src/main/java/com/mina/drones/DronesApplication.java
+++ src/main/java/com/mina/drones/DronesApplication.java
@@ -2,6 +2,7 @@ package com.mina.drones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 public class DronesApplication {
---
+++ src/main/java/com/mina/drones/controllers/DronesController.java
@@ -0,0 +1,12 @@
+package com.mina.drones.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/drones")
+public class DronesController {
+}
